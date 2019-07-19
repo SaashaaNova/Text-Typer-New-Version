abb327cf7bf077e98ca1b337ba9126105bd82836
SasshaaNova <aa.niewinska@gmail.com>
2019-07-19 17:27:54 +0200

    Updates

---
+++ precache-manifest.de1dcf1a13d19811c427eda2d181eb18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8a4cb426a604027229806631796bbd",
+    "url": "/Text-Typer-New-Version/index.html"
+  },
+  {
+    "revision": "ccbca56b8698acad8a10",
+    "url": "/Text-Typer-New-Version/static/css/main.46e23e6b.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/Text-Typer-New-Version/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "ccbca56b8698acad8a10",
+    "url": "/Text-Typer-New-Version/static/js/main.cc7546fd.chunk.js"
+  },
+  {
+    "revision": "37e0e2a92e0f51a07604",
+    "url": "/Text-Typer-New-Version/static/js/runtime~main.7ca64cb2.js"
+  }
+]);
